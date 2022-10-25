303fa19decf05508a23bbc77c0840e3983c5389a
Vinayak Bagaria <workvinayak95@gmail.com>
2022-10-25 16:57:51 +0530

    Add resolver file

--- api/go.mod
+++ api/go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/andybalholm/brotli v1.0.0 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
 	github.com/gofiber/fiber v1.14.6 // indirect
 	github.com/gofiber/utils v0.0.10 // indirect
 	github.com/gorilla/schema v1.1.0 // indirect
--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,1 +1,29 @@
 package routes
+
+import (
+	"github.com/VinayakBagaria/url-shortner/database"
+	"github.com/go-redis/redis"
+	"github.com/gofiber/fiber"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+	url := c.Params("url")
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "short not found in db"})
+	}
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "cannot connect to db"})
+	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+
+	rInr.Incr("counter")
+	c.Redirect(value, 301)
+	return nil
+}
